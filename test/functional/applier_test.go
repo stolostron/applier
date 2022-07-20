9937d6ed56e6933e7347d0b8dd21652b2bce26c9
Yang Le <yangle@redhat.com>
2022-07-20 17:33:03 +0800

    upgrade the go libraries to latest version
    Signed-off-by: Yang Le <yangle@redhat.com>

--- test/functional/applier_test.go
+++ test/functional/applier_test.go
@@ -206,10 +206,10 @@ var _ = Describe("Applier", func() {
 func hasCRDs(client clientset.Interface, expectedCRDs []string) (has bool, missingCRDs []string, err error) {
 	missingCRDs = make([]string, 0)
 	has = true
-	clientAPIExtensionV1beta1 := client.ApiextensionsV1beta1()
+	clientAPIExtensionV1 := client.ApiextensionsV1()
 	for _, crd := range expectedCRDs {
 		klog.V(1).Infof("Check if %s exists", crd)
-		_, errGet := clientAPIExtensionV1beta1.CustomResourceDefinitions().Get(context.TODO(), crd, metav1.GetOptions{})
+		_, errGet := clientAPIExtensionV1.CustomResourceDefinitions().Get(context.TODO(), crd, metav1.GetOptions{})
 		if errGet != nil {
 			if errors.IsNotFound(errGet) {
 				missingCRDs = append(missingCRDs, crd)
