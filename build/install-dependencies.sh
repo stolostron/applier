5eb24e351e64b2674c4c41b1aec391cb64eeb3e1
Dominique Vernier <dvernier@redhat.com>
2022-08-01 14:56:03 -0400

    test
    Signed-off-by: Dominique Vernier <dvernier@redhat.com>

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -8,8 +8,6 @@ _ARCH=$(go env GOARCH)
 
 if ! which patter > /dev/null;     then echo "Installing patter ..."; go install github.com/apg/patter@bd185be70ac8aa766084cfa431f6bfbc266795d4; fi
 if ! which gocovmerge > /dev/null; then echo "Installing gocovmerge..."; go install github.com/wadey/gocovmerge@b5bfa59ec0adc420475f97f89b58045c721d761c; fi
-# if ! which patter > /dev/null;     then echo "Installing patter ..."; pushd $(mktemp -d) && GO111MODULE=off go install github.com/apg/patter && popd; fi
-# if ! which gocovmerge > /dev/null; then echo "Installing gocovmerge..."; pushd $(mktemp -d) && GO111MODULE=off go install github.com/wadey/gocovmerge && popd; fi
 
 # Build tools
 
